f200f96d5a46d1fc9a37dd952a0f772c638d84dd
leada1995 <67641679+leada1995@users.noreply.github.com>
2020-06-30 22:52:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,11 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+embed5 = discord.Embed(title="今のボスは", description="***カルキノス***\n" 
+                       "[**リマクリスレイプッコロミズナ**]"
+                       "(https://discordapp.com/channels/716279661033947207/724105654490497055/726397867836702752)\n",
+                       color=0xff0000)
+embed5.set_thumbnail(url="https://gamewith.akamaized.net/article_tools/pricone-re/gacha/93_enemy.png")
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -16,6 +21,9 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
+    
+async def on_message(message):
+    if message.content == 'ムノカマエ':
+        await message.channel.send('╭( ´ ᵕ ` )╮')
 
 bot.run(token)
